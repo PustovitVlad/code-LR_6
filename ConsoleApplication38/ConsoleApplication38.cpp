cc035878a37c3d4cb869de282ee640971f6bccfb
PustovitVlad <v.v.pustovit2000@gmail.com>
2018-12-10 21:32:35 +0200

    Uploading code

---
+++ ConsoleApplication38/ConsoleApplication38.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char x;
+	cout << "Введіть  путктуаційний знак: ";
+	cin >> x;
+	switch (x)
+	{
+	case'-':cout << "(-)-це тире\n"; break;
+	case'.':cout << "(.)-це крапка\n"; break;
+	case':':cout << "(:)-це двокрапка\n"; break;
+	case';':cout << "(;)-це крапка з комою\n"; break;
+	case'...':cout << "(...)-це три крапки\n"; break;
+	case',':cout << "(,)-це кома\n"; break;
+	case'?':cout << "(?)-це знак питання\n"; break;
+	case'!':cout << "(!)-це знак оклику\n"; break;
+	default:cout << "такого розділового знаку не існує\n"; break;
+	}
+	system("pause");
+	return 0;
+}
